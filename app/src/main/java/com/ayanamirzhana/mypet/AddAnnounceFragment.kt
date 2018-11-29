186d118816d333adc7b5584ba7f414450ed1b21d
AyanaMussabayeva <ayana.mussabayeva@gmail.com>
2018-11-29 13:37:13 +0600

    Intent from fragment works!

--- app/src/main/java/com/ayanamirzhana/mypet/AddAnnounceFragment.kt
+++ app/src/main/java/com/ayanamirzhana/mypet/AddAnnounceFragment.kt
@@ -1,17 +1,43 @@
 package com.ayanamirzhana.mypet
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.content.Intent
+import android.widget.Button
+import android.widget.Toast
+
 
 class AddAnnounceFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.fragment_add_announce, container, false)
+
+
+    private var ctx: Context? = null
+    private var self: View? = null
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        ctx = container?.context
+        self = LayoutInflater.from(ctx).inflate(R.layout.fragment_add_announce, container, false)
+        val bDaButton = self?.findViewById<Button>(R.id.chooseFileButton)
+
+        bDaButton?.setOnClickListener {
+            Toast.makeText(ctx, "button works!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(activity, StorageActivity::class.java)
+            startActivity(intent)
+
+        }
+        return self
+    }
+
 
     companion object {
         fun newInstance(): AddAnnounceFragment = AddAnnounceFragment()
     }
+
+
 }
