fc838cd359f5f48e2499ff64683a6f0035a2a1e8
AyanaMussabayeva <ayana.mussabayeva@gmail.com>
2018-11-29 13:51:38 +0600

    Intent from fragment works!

--- app/src/main/java/com/ayanamirzhana/mypet/StorageActivity.kt
+++ app/src/main/java/com/ayanamirzhana/mypet/StorageActivity.kt
@@ -1,12 +1,46 @@
 package com.ayanamirzhana.mypet
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import kotlinx.android.synthetic.main.activity_storage.*
+import java.io.IOException
 
 class StorageActivity : AppCompatActivity() {
+
+    var imgCode = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_storage)
+        photoButton.setOnClickListener {
+            onSelectPhotoButtonClick()
+        }
+    }
+    fun onSelectPhotoButtonClick() {
+        val photoPickerIntent = Intent(Intent.ACTION_PICK)
+        photoPickerIntent.setType("image/*")
+        startActivityForResult(photoPickerIntent, imgCode)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == imgCode) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (data != null) {
+
+                    var uri = data.data
+                    var bitmap: Bitmap
+
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
     }
 }
